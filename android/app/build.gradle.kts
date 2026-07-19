808075676392f911827bba4ce59a2ad1ec6a3517
Ag1rin <mermoapp@gmail.com>
2026-07-19 16:42:43 +0330

    upadte version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,6 +22,7 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -38,6 +39,7 @@ android {
 
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        multiDexEnabled = true
     }
 
 
@@ -73,7 +75,9 @@ android {
 
 
 flutter {
-
     source = "../.."
+}
 
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
